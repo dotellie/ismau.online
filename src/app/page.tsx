fe7e6639ed24455412d8bdc5f66a08e8ecaba889
Ellie Fagerberg <hi@ellie.moe>
2023-02-12 15:00:40 +0100

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,39 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default async function Home() {
+  if (process.env.HEARTBEAT_URL == null) {
+    throw new Error("HEARTBEAT_URL is not set");
+  }
+
+  const heartbeatRes = await fetch(process.env.HEARTBEAT_URL);
+  const heartbeat = (await heartbeatRes.json()) as {
+    heartbeatList: Record<
+      string,
+      Array<{ status: number; time: string; msg: string; ping: number }>
+    >;
+    uptimeList: Record<string, number>;
+  };
+
+  const statusPerKey = Object.fromEntries(
+    Object.entries(heartbeat.heartbeatList)
+      .map(([key, list]) => {
+        const entry = list[list.length - 1];
+        if (!entry) return null;
+        return [key, entry.status] as const;
+      })
+      .filter(<T,>(tuple: T | null): tuple is T => tuple != null)
+  );
+
+  const isMauOnline = Object.values(statusPerKey).every(
+    (status) => status >= 1
+  );
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className={[styles.main, inter.className].join(" ")}>
+      <p>Mau is {isMauOnline ? "online" : "offline"}</p>
     </main>
-  )
+  );
 }
