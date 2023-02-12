1607f207d1a0da1fe557bd859c060672958d0698
Arvid Berndtsson <arvid@berndtsson.me>
2023-02-12 18:18:58 +0100

    Added a pulsating button to the middle of the page and a query for debugging (offline=true).

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,41 @@
 import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
+import { PageSpin } from "./page-spin";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default async function Home() {
-  if (process.env.HEARTBEAT_URL == null) {
-    throw new Error("HEARTBEAT_URL is not set");
-  }
+export default async function Home(props: {
+    searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+    const forceOffline = props.searchParams?.offline === "true";
 
-  const heartbeatRes = await fetch(process.env.HEARTBEAT_URL, {
-    next: { revalidate: 10 },
-  });
-  const heartbeat = (await heartbeatRes.json()) as {
-    heartbeatList: Record<
-      string,
-      Array<{ status: number; time: string; msg: string; ping: number }>
-    >;
-    uptimeList: Record<string, number>;
-  };
+    if (process.env.HEARTBEAT_URL == null) {
+        throw new Error("HEARTBEAT_URL is not set");
+    }
 
-  const statusPerKey = Object.fromEntries(
-    Object.entries(heartbeat.heartbeatList)
-      .map(([key, list]) => {
-        const entry = list[list.length - 1];
-        if (!entry) return null;
-        return [key, entry.status] as const;
-      })
-      .filter(<T,>(tuple: T | null): tuple is T => tuple != null)
-  );
+    const heartbeatRes = await fetch(process.env.HEARTBEAT_URL, {
+      next: { revalidate: 10 },
+    });
+    const heartbeat = (await heartbeatRes.json()) as {
+        heartbeatList: Record<
+            string,
+            Array<{ status: number; time: string; msg: string; ping: number }>
+        >;
+        uptimeList: Record<string, number>;
+    };
 
-  const isMauOnline = Object.values(statusPerKey).every(
-    (status) => status >= 1
-  );
+    const statusPerKey = Object.fromEntries(
+        Object.entries(heartbeat.heartbeatList)
+            .map(([key, list]) => {
+                const entry = list[list.length - 1];
+                if (!entry) return null;
+                return [key, entry.status] as const;
+            })
+            .filter(<T,>(tuple: T | null): tuple is T => tuple != null)
+    );
 
-  return (
-    <main className={[styles.main, inter.className].join(" ")}>
-      <p>Mau is {isMauOnline ? "online" : "offline"}</p>
-    </main>
-  );
+    const isMauOnline = forceOffline
+        ? false
+        : Object.values(statusPerKey).every((status) => status >= 1);
+
+    return <PageSpin isMauOnline={isMauOnline} />;
 }
